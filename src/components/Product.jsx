0e1e5c25ecfbbb33c6f807aae29510481d0011f5
badnaim <batnyaam.tumurbat@gmail.com>
2023-02-01 12:43:32 +0800

    b

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import Img7 from "../images/Img7";
 import "../styles/product.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import EditSec from "../components/subcomps/EditSec";
+import axios from "axios";
 
 export default function Product({ detail }) {
-  // function deleteProductHandler(id) {
-  //   axios.delete(`http://localhost:2020/product/${id}`);
-  // }
+  function deleteProductHandler(id) {
+    axios.delete(`http://localhost:2020/product/${id}`);
+  }
+
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+
+  function deleteProductHandler(id) {
+    axios.delete(`http://localhost:2020/products/delete/${id}`);
+  }
   return (
     <div className="productOfAdmin">
       <div id="box">
@@ -35,8 +54,38 @@ export default function Product({ detail }) {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Өөрчлөх</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Устгах</Dropdown.Item>
+              <Dropdown.Item onClick={handleShow1}>Өөрчлөх</Dropdown.Item>
+              <Offcanvas placement="bottom" show={show1} onHide={handleClose1}>
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <EditSec />
+                </Offcanvas.Body>
+              </Offcanvas>
+
+              <Dropdown.Item onClick={handleShow}>Устгах</Dropdown.Item>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  {/* <Modal.Title>Modal heading</Modal.Title> */}
+                </Modal.Header>
+                <Modal.Body>Устгахдаа итгэлтэй байна уу бяач?</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      deleteProductHandler(detail.id);
+                      console.log(detail.id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+
               <Dropdown.Item href="#/action-3">Вебсайтаас нуух</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
